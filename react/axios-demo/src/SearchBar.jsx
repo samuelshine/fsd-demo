4c6766c20ecd4610d99907176a8cdc2969744e2e
Samuel Shine <122368259+samuelshine@users.noreply.github.com>
2025-08-01 09:52:28 +0530

    Update SearchBar.jsx

--- react/axios-demo/src/SearchBar.jsx
+++ react/axios-demo/src/SearchBar.jsx
@@ -15,10 +15,10 @@ function SearchBar({onSubmit}){
     }
     
     return(
-        <div className="search-bar flex items-center">
-            <form className="flex flex-col items-center p-20 w-screen bg-sky-300" onSubmit={handleFormSubmit}>
+        <div className="search-bar flex sticky top-0 items-center">
+            <form className="flex flex-col items-center p-5 w-screen bg-sky-300" onSubmit={handleFormSubmit}>
                 <label className="p-2 text-4xl font-bold">Enter Search Term</label>
-                <input placeholder="Search..."className="text-lg border-radius-l text-center border-2 border-black" onChange={handleChange} value={term} />
+                <input placeholder="Search..."className="text-lg text-center border-2 rounded-lg border-black" onChange={handleChange} value={term} />
             </form>
         </div>
     )
